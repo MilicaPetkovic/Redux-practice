7b37bb5ec5a9ffa9f4594a10c8c431805a2dc352
user <user@tana.uon>
2019-05-25 17:33:00 +0200

    styling added

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './App.css';
 
-const Container = () => {
-  return (
+class Container extends Component {
+  render() {
+    return (
     <div style={{height: "100vh", textAlign: "center"}}>
-      <h3>For David freedom is the ability to find peace inward.</h3>
+      <p>25.5.2017 New York</p>
+      <h3><span> For </span> David <span>freedom</span> is the ability to find peace inward.</h3>
       <hr/>
-      <h3>For David freedom is the ability to find peace inward.</h3>
+      <p>25.5.2017 New York</p>
+      <h3><span> For </span> David <span>freedom</span> is the ability to find peace inward.</h3>
       <hr/>
-      <h3>For David freedom is the ability to find peace inward.</h3>
+      <p>25.5.2017 New York</p>
+      <h3><span> For </span> David <span>freedom</span> is the ability to find peace inward.</h3>
       <hr/>
-      <h3>For David freedom is the ability to find peace inward.</h3>
+      <p>25.5.2017 New York</p>
+      <h3><span> For </span> David <span>freedom</span> is the ability to find peace inward.</h3>
       <hr/>
     </div>
   )
+  }
 }
 
-export default Container
+export default Container;
